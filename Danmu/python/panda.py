a257a95edcbf1203e5a7b9772ee92ec142c54867
Mod <boningga@usc.edu>
2017-05-03 10:06:16 -0700

    automatic

--- Danmu/python/panda.py
+++ Danmu/python/panda.py
@@ -11,6 +11,7 @@ import requests
 
 CHATINFOURL = 'http://riven.panda.tv/chatroom/getinfo?roomid='
 CHATROOMAPI = 'http://room.api.m.panda.tv/index.php?method=room.shareapi&roomid='
+LOGFILEDIR = '~/pandaLog/'
 IGNORE_LEN = 12
 META_LEN = 4
 CHECK_LEN = 4
@@ -30,38 +31,77 @@ HOSTER = '90'
 OFFLINE_STATUS = '3'
 ONLINE_STATUS = '2'
 
-def loadInit() -> []:
-    roomid = []
+DANMU_DICT = {}
+AUDITION_DICT = {}
+ONLINE_FLAGS = {}
+
+class DanmuThread(threading.Thread):
+    def __init__(self, roomID, name):
+        threading.Thread.__init__(self)
+        self.roomID = roomID
+        self.name = name
+    def run(self):
+        print("===========DanmuThread on {} starts===========".format(self.name))
+
+        filename = str(self.roomID) + "_" + self.name + ".csv"
+        logdir = os.path.expanduser(LOGFILEDIR)
+        # getChatInfo(self.roomID)
+        if os.path.isfile(logdir + filename):
+            logfile = open(logdir + filename, 'a')
+        else:
+            logfile = open(logdir + filename, 'w')
+            logfile.write("time, danmu_number, audition_number\n")
+        while room_is_online(self.roomID):
+
+            start_time = time.time()
+            DANMU_DICT[self.roomID] = 0
+            time.sleep(30)
+            logfile.write("{},{},{}\n".format(time.ctime(start_time), DANMU_DICT[self.roomID], \
+                                                  AUDITION_DICT[self.roomID]))
+            print("{},{},{}\n".format(time.ctime(start_time), DANMU_DICT[self.roomID], \
+                                          AUDITION_DICT[self.roomID]))
+
+        ONLINE_FLAGS[self.roomID] = False
+        logfile.close()
+        print("===========Thread on {} ends===========".format(self.name))
+
+
+def loadInit()->[]:
+    roomInfos = []
     with open(INIT_PROPERTIES, 'r') as f:
         init = f.read()
         init = init.split('\n')
         for line in init:
             if len(line.split(':')) == 2:
-                roomid.append(line.split(':')[1])
-    return roomid
+                #[(roomID, name)]
+                roomInfos.append((line.split(':')[1].split('#')[0], line.split(':')[1].split('#')[1]))
+    for (id, name) in roomInfos:
+        ONLINE_FLAGS[id] = False
+        AUDITION_DICT[id] = 0
+    return roomInfos
 
-def notify(title, message):
-    if SYSINFO == 'Windows':
-        pass
-    elif SYSINFO == 'Linux':
-        os.system('notify-send {}'.format(': '.join([title, message])))
-    else:   #for mac
-        #t = '-title {!r}'.format(title)
-        #m = '-message {!r}'.format(message)
-        #os.system('terminal-notifier {} -sound default'.format(' '.join([m, t])))
-        print(message)
 
-def getRoomInfo(roomid):
+def add_danmu(roomid):
+    DANMU_DICT[roomid] += 1
+
+def update_audition(roomid, audition_num):
+    AUDITION_DICT[roomid] = audition_num
+
+def room_is_online(room_id):
     r = requests.get('http://room.api.m.panda.tv/index.php?\
-                     method=room.shareapi&roomid=' + str(roomid))
-
+                     method=room.shareapi&roomid=' + str(room_id))
     status = r.json()['data']['roominfo']['status']
+    # TODO: Understand what does status 1 means
     if status == OFFLINE_STATUS:
-        print("roomid {} is offline".format(roomid))
-    elif status == ONLINE_STATUS:
-        print("roomid {} is online".format(roomid))
+        return False
+    else:
+        return True
 
-def getChatInfo(roomid):
+
+def getChatInfo(roomid, name):
+    print("===========getChatInfo on {} starts===========".format(name))
+    thread = DanmuThread(roomid, name)
+    thread.start()
     with urllib.request.urlopen(CHATINFOURL + roomid) as f:
         data = f.read().decode('utf-8')
         chatInfo = json.loads(data)
@@ -75,7 +115,8 @@ def getChatInfo(roomid):
         authtype = str(chatInfo['data']['authType']).encode('utf-8')
         sign     = str(chatInfo['data']['sign']).encode('utf-8')
         ts       = str(chatInfo['data']['ts']).encode('utf-8')
-        msg  = b'u:' + rid + b'@' + appid + b'\nk:1\nt:300\nts:' + ts + b'\nsign:' + sign + b'\nauthtype:' + authtype
+        msg  = b'u:' + rid + b'@' + appid + b'\nk:1\nt:300\nts:' + ts\
+        + b'\nsign:' + sign + b'\nauthtype:' + authtype
         msgLen = len(msg)
         sendMsg = FIRST_REQ + int.to_bytes(msgLen, 2, 'big') + msg
         s.sendall(sendMsg)
@@ -85,25 +126,26 @@ def getChatInfo(roomid):
             recvLen = int.from_bytes(s.recv(2), 'big')
             s.recv(recvLen)
         def keepalive():
-            while True:
+            while ONLINE_FLAGS[roomid]:
                 #print('================keepalive=================')
                 s.send(KEEPALIVE)
                 time.sleep(150)
         threading.Thread(target=keepalive).start()
 
-        while True:
+        while ONLINE_FLAGS[roomid]:
             recvMsg = s.recv(CHECK_LEN)
             if recvMsg == RECVMSG:
                 recvLen = int.from_bytes(s.recv(2), 'big')
                 recvMsg = s.recv(recvLen)   #ack:0
                 totalLen = int.from_bytes(s.recv(META_LEN), 'big')
                 try:
-                    analyseMsg(s, totalLen)
+                    analyseMsg(s, totalLen, roomid)
                 except Exception as e:
                     pass
+    print("===========getChatInfo on {} ends===========".format(name))
 
 
-def analyseMsg(s, totalLen):
+def analyseMsg(s, totalLen, roomid):
     while totalLen > 0:
         s.recv(IGNORE_LEN)
         recvLen = int.from_bytes(s.recv(META_LEN), 'big')
@@ -111,10 +153,11 @@ def analyseMsg(s, totalLen):
         # recv the whole msg.
         while recvLen > len(recvMsg):
             recvMsg = b''.join(recvMsg, s.recv(recvLen - len(recvMsg)))
-        formatMsg(recvMsg)
+        formatMsg(recvMsg, roomid)
         totalLen = totalLen - IGNORE_LEN - META_LEN - recvLen
 
-def formatMsg(recvMsg):
+
+def formatMsg(recvMsg, roomid):
     try:
         jsonMsg = eval(recvMsg)
         content = jsonMsg['data']['content']
@@ -135,31 +178,35 @@ def formatMsg(recvMsg):
             emoji = re.match(r"(.*)\[:(.*)](.*)", content)
             if emoji:
                 content = emoji.group(1) + '*' + emoji.group(2) + '*' + emoji.group(3)
-            print(nickName + ":" + content)
-            notify(nickName, content)
-        elif jsonMsg['type'] == BAMBOO_TYPE:
-            nickName = jsonMsg['data']['from']['nickName']
-            print(nickName + "送给主播[" + content + "]个竹子")
-            notify(nickName, "送给主播[" + content + "]个竹子")
-        elif jsonMsg['type'] == TU_HAO_TYPE:
-            nickName = jsonMsg['data']['from']['nickName']
-            price = jsonMsg['data']['content']['price']
-            print('*********' + nickName + "送给主播[" + price + "]个猫币" + '**********')
-            notify(nickName, "送给主播[" + price + "]个猫币")
+            # print(nickName + ":" + content)
+            add_danmu(roomid)
+        # elif jsonMsg['type'] == BAMBOO_TYPE:
+        #     nickName = jsonMsg['data']['from']['nickName']
+        #     print(nickName + "送给主播[" + content + "]个竹子")
+        #     notify(nickName, "送给主播[" + content + "]个竹子")
+        # elif jsonMsg['type'] == TU_HAO_TYPE:
+        #     nickName = jsonMsg['data']['from']['nickName']
+        #     price = jsonMsg['data']['content']['price']
+        #     print('*********' + nickName + "送给主播[" + price + "]个猫币" + '**********')
+        #     notify(nickName, "送给主播[" + price + "]个猫币")
         elif jsonMsg['type'] == AUDIENCE_TYPE:
             print('===========观众人数' + content + '==========')
+            update_audition(roomid, content)
         else:
             pass
     except Exception as e:
-        print(recvMsg)
+        # print(recvMsg)
         pass
 
 
 def main():
-    roomid = loadInit()
-    for r in roomid:
-        getRoomInfo(r)
-    #getChatInfo(roomid[0])
+    roomInfos = loadInit()
+    while True:
+        for (id, name) in roomInfos:
+            if not ONLINE_FLAGS[id] and room_is_online(id):
+                ONLINE_FLAGS[id] = True
+                threading.Thread(target=getChatInfo, args=(id, name)).start()
+
 
 if __name__ == '__main__':
     main()
