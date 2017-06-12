45a3924e8c41b6a6c5ce8f73f7c4694524160bc4
Mod <boningga@usc.edu>
2017-06-12 01:05:03 -0700

    add append function

--- dl_analyse/dl_danmu/__init__.py
+++ dl_analyse/dl_danmu/__init__.py
@@ -59,14 +59,14 @@ class DanmuThread(threading.Thread):
         self.__client       = None
         self.__record_id    = ""
         self.__live_status_rescan_interval = live_status_rescan_interval
-        self.danmuCounter = DanmuCounter(name)
+        self.__dc = DanmuCounter(name)
         self.__url = 'http://' + PlatformUrl_[self.__platform] + self.__room_id
         client_dict = {'panda': PandaDanMuClient,
                        'douyu': DouYuDanMuClient}
         if platform not in client_dict.keys():
             raise KeyError
         self.__baseClient = client_dict[platform]
-        self.__client = self.__baseClient(self.__room_id, self.__name, self.danmuCounter.count_danmu)
+        self.__client = self.__baseClient(self.__room_id, self.__name, self.__dc.count_danmu)
 
     def room_is_live(self):
         return self.__client.get_live_status()
@@ -88,7 +88,7 @@ class DanmuThread(threading.Thread):
         self.__should_stop = True
 
     def gan(self):
-        print("===========DanmuThread on {} starts===========".format(self.__name, self.__room_id))
+        print("===========DanmuThread on {}({}) starts===========".format(self.__name, self.__room_id))
         f = open('danmu_log', 'a')
         f.write("===========DanmuThread on {}({}) starts===========\n".format(self.__name, self.__room_id))
         try:
@@ -125,15 +125,16 @@ class DanmuThread(threading.Thread):
         block_id = 0
         logfile = open(log_dir + statistic_filename, 'w')
         logfile.write("time, block, danmu, 666, 学不来, 逗鱼时刻\n")
+        l_last_block_data = (False, "", (0, 0), (0, 0, 0, 0)) # (is_processed, old_name, (block), (d,s,t,l))
         while not self.__should_stop:
-            self.danmuCounter.add_block()
+            self.__dc.add_block()
             block_start_time = int(time.time())  # For record
             block_end_time = start_time + Block_Size_In_Second_ * (block_id + 1)  # For calculating sleeping_time
             sleep_time = block_end_time - time.time()
             # print('{}\'s wait time is :{}'.format(self.__name, sleep_time))
             time.sleep(sleep_time)
 
-            count_res = (self.danmuCounter.get_count())
+            count_res = (self.__dc.get_count())
             try:
                 logfile.write("{},{},{},{},{},{}\n".format(block_start_time, block_id, *count_res))
                 print("{}'s({}) logfile: time:{}, block:{}, danmu:{}, 666:{}, gou:{}, douyu:{}"
@@ -145,31 +146,37 @@ class DanmuThread(threading.Thread):
             # print('{}\'s current block_id is {}'.format(self.__name, block_id))
 
             if Record_Mode_ and block_id >= 3:
-                print("{}'({}) douyu_list has {} times and target number is {}".format(self.__name, self.__record_id,
-                                                                      sum(i >= 2 for i in self.danmuCounter.DouyuList),
-                                                                            self.danmuCounter.DouyuList[block_id - 1]))
-                if self.danmuCounter.DouyuList[block_id - 1] > 1:
-                    l_count = self.danmuCounter.get_count(block_id - 1)
-                    saved_video_name = '{}_douyu{}_block{}to{}_lucky{}_triple{}' \
-                        .format(self.name, l_count.douyu, block_id - 3, block_id, l_count.lucky, l_count.triple)
-                    threading.Thread(target=record.combine_block,
-                                     args=(self.__record_id, block_id - 3, block_id, saved_video_name)).start()
-                elif self.danmuCounter.get_score(block_id - 1) >= ScoreThreshold_:
-                    l_count = self.danmuCounter.get_count(block_id - 1)
-                    saved_video_name = '{}_score{}_block{}to{}_douyu{}_triple{}_lucky{}' \
-                        .format(self.name, self.danmuCounter.get_score(block_id - 1), l_count.douyu, block_id - 3,
-                                block_id, l_count.douyu, l_count.triple, l_count.lucky)
+                print("{}'({}) has {} douyu times and target number is {}".
+                      format(self.__name, self.__record_id, sum(i >= 2 for i in self.__dc.DouyuList),
+                             self.__dc.DouyuList[block_id - 1]))
+                if self.__dc.get_score(-1) >= ScoreThreshold_ or self.__dc.DouyuList[-1] > 1:
+                    if l_last_block_data[0]:
+                        l_c = self.__dc.get_count(block_id - 1)
+                        l_video_name = '{}_d{}_b{}to{}_s{}_t{}_l{}'.\
+                            format(self.name, l_c.douyu + l_last_block_data[3][0], l_last_block_data[2][0],
+                                   l_last_block_data[2][1], self.__dc.get_score(-1)+l_last_block_data[3][1],
+                                   l_last_block_data[3][2] + l_c.triple, l_last_block_data[3][3] + l_c.lucky)
+                        threading.Thread(target=record.append_block,
+                                         args=(self.__record_id, block_id, l_last_block_data[1], l_video_name))
+                        l_last_block_data = (True, l_video_name, (l_last_block_data[2][0], block_id),
+                                         (l_last_block_data[3][0] + l_c.douyu,
+                                          l_last_block_data[3][1] + self.__dc.get_score(-1),
+                                          l_last_block_data[3][2] + l_c.triple,
+                                          l_last_block_data[3][3] + l_c.lucky))
+                    else:
+                        l_c = self.__dc.get_count(block_id - 1)
+                        l_video_name = '{}_d{}_b{}to{}_s{}_t{}_l{}' \
+                            .format(self.name, l_c.douyu, block_id - 3, block_id, self.__dc.get_score(-1),
+                                    l_c.triple, l_c.lucky)
+                        l_last_block_data = (True, l_video_name, (block_id - 3, block_id),
+                                             (l_c.douyu, self.__dc.get_score(-1), l_c.triple, l_c.lucky))
+                        threading.Thread(target=record.combine_block,
+                                         args=(self.__record_id, block_id - 3, block_id, l_video_name)).start()
+                else:
+                    l_last_block_data = (False, "")
+                threading.Thread(target=record.delete_block, args=(self.__record_id, block_id - 3, block_id - 3)).\
+                    start()
 
-                    threading.Thread(target=record.combine_block,
-                                     args=(self.__record_id, block_id - 3, block_id, saved_video_name)).start()
-                if block_id == 3:
-                    if self.danmuCounter.DouyuList[block_id - 2] < 2:
-                        threading.Thread(target=record.delete_block,
-                                     args=(self.__record_id, 0, 0)).start()
-                elif self.danmuCounter.DouyuList[block_id - 4] < 2 and self.danmuCounter.DouyuList[block_id - 3] < 2 \
-                        and self.danmuCounter.DouyuList[block_id - 2] < 2:
-                    threading.Thread(target=record.delete_block,
-                                     args=(self.__record_id, block_id - 3, block_id - 3)).start()
             block_id += 1
         self.__is_running = False
         logfile.close()
