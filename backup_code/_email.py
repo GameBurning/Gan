ac20de3664234f3d3227a1ab2bdb455842b85bf5
Mod <boningga@usc.edu>
2017-07-14 19:25:49 -0700

    test email

--- backup_code/_email.py
+++ backup_code/_email.py
@@ -2,7 +2,7 @@ import smtplib
 
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-server.login("sdi.gbn@gmail.com", "983556Xd")
+server.login("sdi.gbn@gmail.com", "")
 
 msg = "Baobao!"
 server.sendmail("sdi.gbn@gmail.com", "boningga@usc.edu", msg)
